3df36068484382f4ad5e3835d0ff99ddb91cf1c2
Jonathan <jonathan.grangien@gmail.com>
2015-08-11 00:41:46 +0200

    implement router

--- js/app.js
+++ js/app.js
@@ -1,7 +1,23 @@
-angular.module('portfolioApp', [])
-  .controller('PresentationController', function() {
-    var presentation = this;
-    
-    presentation.text = "Jonathan";
- 
-  });
+angular.module('portfolioApp', ["ngRoute"])
+	.controller('PresentationController', function($scope) {
+		$scope.text = "Jonathan";
+	})
+
+	.controller('ProjectsController', function($scope) {
+		$scope.text = "Such projects much show";
+	})
+
+	.config(["$routeProvider", function($routeProvider) {
+		$routeProvider 
+		.when('/', { 
+		  controller: 'PresentationController', 
+		  templateUrl: 'views/presentation.html' 
+		})
+		.when('/projects', {
+			controller: 'ProjectsController',
+			templateUrl: 'views/projects.html'
+		})
+		.otherwise({ 
+		  redirectTo: '/' 
+		}); 
+	}]);
