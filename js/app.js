bb274cb65eca7279ce2a4d3ad86b001ccf0a3a32
Jonathan <jonathan.grangien@gmail.com>
2015-08-12 01:33:41 +0200

    Implement click handling for header bar items

--- js/app.js
+++ js/app.js
@@ -1,10 +1,24 @@
 angular.module('portfolioApp', ["ngRoute"])
 	.controller('PresentationController', function($scope) {
 		$scope.text = "Jonathan";
+
+		var toggleSelected = function() {
+			$('#projects-link').removeClass("pure-menu-selected");
+			$('#presentation-link').addClass("pure-menu-selected");
+		}
+
+		toggleSelected();
 	})
 
 	.controller('ProjectsController', function($scope) {
 		$scope.text = "Such projects much show";
+
+		var toggleSelected = function() {
+			$('#presentation-link').removeClass("pure-menu-selected");
+			$('#projects-link').addClass("pure-menu-selected");
+		}
+
+		toggleSelected();
 	})
 
 	.config(["$routeProvider", function($routeProvider) {
