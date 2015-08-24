35eb40d40d3095e7bfebd66fa1ee805f35989678
Jonathan <jonathan.grangien@gmail.com>
2015-08-24 15:01:35 +0200

    Add down scaled image for xs view

--- index.php
+++ index.php
@@ -85,8 +85,11 @@
 
 							<p>Meatloaf ham hock kielbasa tenderloin t-bone fatback landjaeger shankle sausage ball tip pancetta tri-tip hamburger venison. Beef ball tip sirloin tenderloin. Shank brisket jowl tail kevin leberkas shankle landjaeger kielbasa drumstick tongue.</p>
 						</div>
-						<div class="col-md-5 pull-right">
-							<img src="img/seriousman.jpg" class="hidden-xs img-circle" />
+						<div class="col-md-5 pull-right hidden-xs">
+							<img src="img/seriousman.jpg" class="img-circle" />
+						</div>
+						<div class="col-xs-8 col-xs-offset-2 visible-xs text-center">
+							<img src="img/seriousman.jpg" style="width: 100%;" class="img-circle" />
 						</div>
 					</div>
 				</div>
